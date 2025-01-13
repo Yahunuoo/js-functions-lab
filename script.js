ca6630751c8f15437becb825920fb76fcd9bbcd3
User Name <muhanna.juma@outlook.com>
2025-01-13 08:26:16 +0300

    new commit

--- script.js
+++ script.js
@@ -41,17 +41,29 @@ console.log('Exercise 3 Result:', isCharAVowel('i'));
 
 
 
-/*
-const generateEmail=(name, domain) => {
-    const email = name + domain;
-    return email;
-}
-console.log('Exercise 4 Result:', generateEmail("johnsmith", "@example.com"));
-*/
-
-function generateEmail(name, domin){
+function generateEmail_2(name, domin){
     let sum = name + domin;
     return sum
 }
-console.log('Exercise 4 Result:', generateEmail("johnsmith", "@example.com"));
+console.log('Exercise 4 Result:', generateEmail_2("johnsmith", "@example.com"));
 
+
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, time){
+    let sum = name + time;
+    return sum
+}
+
+console.log('Exercise 5 Result:', greetUser("Good morning, ", "Sam"));
--- test-by-me.js
+++ test-by-me.js
@@ -8,3 +8,9 @@ while (i < 10) {
   i++;
   console.log(i)
 }
+
+const generateEmail=(name, domain) => {
+  const email = name + domain;
+  return email;
+}
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "@example.com"));
