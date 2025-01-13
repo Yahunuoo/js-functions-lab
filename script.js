1aea05528f3a5579f8c0654b9b3e456eb0d97bda
User Name <muhanna.juma@outlook.com>
2025-01-13 08:28:31 +0300

    new commit

--- script.js
+++ script.js
@@ -49,18 +49,6 @@ console.log('Exercise 4 Result:', generateEmail_2("johnsmith", "@example.com"));
 
 
 
-/*
-Exercise 5: greetUser()
-
-Define a function called greetUser. It should take a name and a 
-time of day (morning, afternoon, evening) and return a personalized 
-greeting.
-
-Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
-
-Complete the exercise in the space below:
-*/
-
 function greetUser(name, time){
     let sum = name + time;
     return sum
